07241c446700e7a1762c4ab15621c61ab0dd6416
Lucas Santi <idlucassanti@hotmail.com>
2024-05-25 16:14:30 -0300

    #9 [StatusBar] Inclusão e estilização do StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
@@ -11,6 +12,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {
         fontsLoaded ? <Groups /> : <Loading />
       }
