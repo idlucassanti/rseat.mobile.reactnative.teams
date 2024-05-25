f3d806e127a36e867947f4214a60feff6e1eafd4
Lucas Santi <idlucassanti@hotmail.com>
2024-05-25 15:44:10 -0300

    #8 [Componente] Criação do componente Loading com ActivityIndicator do React Native

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { ActivityIndicator } from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {
-        fontsLoaded ? <Groups /> : <ActivityIndicator />
+        fontsLoaded ? <Groups /> : <Loading />
       }
       
     </ThemeProvider>
