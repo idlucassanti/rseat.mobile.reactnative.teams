f9dd893815656d28c3d3c3f8409d0bb119e6706a
Lucas Santi <idlucassanti@hotmail.com>
2024-05-25 14:01:19 -0300

    #7 [Estilização] Inclusão de Fonte personalizada do google fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+import { ActivityIndicator } from "react-native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {
+        fontsLoaded ? <Groups /> : <ActivityIndicator />
+      }
+      
     </ThemeProvider>
   );
 }
