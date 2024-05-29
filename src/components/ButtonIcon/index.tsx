7152206d42ee28e51196092f8f863e9c3a32ffca
Lucas Santi <idlucassanti@hotmail.com>
2024-05-29 01:26:52 -0300

    #25 [ButtonIcon] Refatoração do componente ButtonIcon para permitir que Ícone seja dinâmico através da tipagem

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,18 @@
 import { TouchableOpacityProps } from "react-native";
-import { Container, Icon } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
 
 type Props = TouchableOpacityProps & {
-
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
 }
 
-export function ButtonIcon({ } :Props) {
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest } :Props) {
   return (
-    <Container>
+    <Container {...rest}>
       <Icon
-        name="home"
-        type="PRIMARY"
+        name={icon}
+        type={type}
       />
     </Container>
   );
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,15 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players() {
   return (
     <Container>
       <Header showButtonBack />
       <Highlight title="Nome da turma" subtitle="adicione a galera e separe os esportes"/>
+
+      <ButtonIcon icon="home"/>
     </Container>
   );
 }
