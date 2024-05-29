da0819b0de9df20101d3c64bb735e842fd484f7f
Lucas Santi <idlucassanti@hotmail.com>
2024-05-29 02:08:45 -0300

    #27 [Filter] Criação do componente Filter com controle de isActive

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "./components/Filter";
 
 export function Players() {
   return (
@@ -14,6 +15,8 @@ export function Players() {
         <Input placeholder="Nome do participante"/>
         <ButtonIcon icon="add"/>
       </Form>
+
+      <Filter title="Time A" isActive />
     </Container>
   );
 }
